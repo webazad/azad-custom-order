38d11401f0865e916b323ddc2958f0549899563a
Azad <azad@azad.com>
2020-02-10 18:29:42 +0600

    Another

---
+++ azad-custom-order.php
@@ -0,0 +1,18 @@
+<?php
+/* 
+Plugin Name: Azad Custom Order
+Description: A very simple plugin to order your posts in a way you want
+Plugin URi: gittechs.com/plugin/azad-popular-posts
+Author: Md. Abul Kalam Azad
+Author URI: gittechs.com/author
+Author Email: webdevazad@gmail.com
+Version: 1.0.0.
+Text Domain: azad-custom-order
+*/
+
+defined('ABSPATH') || exit;
+
+class Azad_Custom_Order{
+    public function __construct(){}
+    public function __destruct(){}
+}
